2b25693de26c3ace4e3089cce462f0ebe18e850c
Nilotpal Pramanik <33095476+goodday451999@users.noreply.github.com>
2018-01-28 12:57:24 +0530

    Add file via upload

---
+++ circle_square.py
@@ -0,0 +1,16 @@
+import turtle
+
+my_turtle = turtle.Turtle()
+my_turtle.speed(0)
+
+
+def square(length,angle):
+    for i in range(4):
+        my_turtle.forward(length)
+        my_turtle.left(angle)
+        
+for i in range(360):
+    square(200, 90)
+    my_turtle.left(11)
+
+    
